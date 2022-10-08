f8efa670558b5dc574cc309a0c635153d5ba0050
JeffRisberg <j.s.risberg@gmail.com>
2022-10-07 21:00:50 -0700

    more endpoints

--- src/server.py
+++ src/server.py
@@ -10,3 +10,11 @@ async def read_item(item_id):
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items2/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
